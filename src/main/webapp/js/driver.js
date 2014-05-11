b05face48e94cae2ef3d60ab4cfd7dd954d1127a
Jenny <jdommett@gmail.com>
2014-05-11 19:57:38 +0100

    automatic movement on the UI with pause, speed up and slow down. Log gets cleared when maze is started

--- src/main/webapp/js/driver.js
+++ src/main/webapp/js/driver.js
@@ -8,11 +8,68 @@ function Driver(theExplorer, theGraphics, theMaze)
     var graphics = theGraphics;
     var maze = theMaze;
     var explorerLocation = null;
+    var isMoving = false;
+    var defaultMoveDelayMs = 500;
+    var moveDelayMs = defaultMoveDelayMs;
 
     var startMazeTraversal = function startMazeTraversal()
     {
         $(".traversalButton").removeAttr("disabled");
         graphics.redrawCurrentMaze();
+        moveDelayMs = defaultMoveDelayMs;
+        LOG.clear();
+    };
+
+
+    var moveCycle = function moveCycle()
+    {
+        if(!isMoving)
+        {
+            return;
+        }
+
+        var theChosenDirection = null;
+        var theMoveOutcome = null;
+
+        maze.getAvailableExits(explorerLocation)
+            .then(function afterExits(exits)
+            {
+                return explorer.whichWay(explorerLocation, exits);
+            })
+            .then(function afterWhichWay(chosenDirection)
+            {
+                theChosenDirection = chosenDirection;
+                return maze.attemptMazeMove(explorerLocation, chosenDirection);
+            })
+            .then(function afterAttemptMove(outcomeJSON)
+            {
+                theMoveOutcome = $.parseJSON(outcomeJSON);
+                return explorer.moveExplorer(explorerLocation, theMoveOutcome.location);
+            })
+            .then(function afterMoveExplorer()
+            {
+                graphics.drawExplorerLocation(explorerLocation, theMoveOutcome.location);
+                explorerLocation = theMoveOutcome.location;
+                LOG.updateLog("-------------------------");
+
+                if(theMoveOutcome.exitReached)
+                {
+                    LOG.updateLog("Exit reached!");
+                    explorer.exitMaze();
+                    endMazeTraversal();
+                }
+                else
+                {
+                    setTimeout(moveCycle, moveDelayMs);
+                }
+
+            })
+            .fail(function fail(err)
+            {
+                LOG.updateLog("Failed to move " + JSON.stringify(chosenDirection) + " from " + JSON.stringify(explorerLocation));
+                LOG.updateLog("-------------------------");
+            })
+            .done();
     };
 
     var endMazeTraversal = function endMazeTraversal()
@@ -27,6 +84,7 @@ function Driver(theExplorer, theGraphics, theMaze)
     this.load = function load()
     {
         var thisDriver = this;
+        $("#pauseButton").hide();
         graphics.loadSprites();
 
         maze.getMazes()
@@ -58,6 +116,7 @@ function Driver(theExplorer, theGraphics, theMaze)
     this.startMaze = function startMaze()
     {
         var mazeEntrance = null;
+        this.stopMoving();
 
         explorer.getName()
         .then(function afterGetName(name)
@@ -83,45 +142,25 @@ function Driver(theExplorer, theGraphics, theMaze)
         .done();
     };
 
-    this.moveCycle = function moveCycle()
+    this.startMoving = function startMoving()
     {
-        var theChosenDirection = null;
-        var theMoveOutcome = null;
+        $("#goButton").hide();
+        $("#pauseButton").show();
+        isMoving = true;
+        moveCycle();
+    }
 
-        maze.getAvailableExits(explorerLocation)
-        .then(function afterExits(exits)
-        {
-            return explorer.whichWay(explorerLocation, exits);
-        })
-        .then(function afterWhichWay(chosenDirection)
-        {
-            theChosenDirection = chosenDirection;
-            return maze.attemptMazeMove(explorerLocation, chosenDirection);
-        })
-        .then(function afterAttemptMove(outcomeJSON)
-        {
-            theMoveOutcome = $.parseJSON(outcomeJSON);
-            return explorer.moveExplorer(explorerLocation, theMoveOutcome.location);
-        })
-        .then(function afterMoveExplorer()
-        {
-            graphics.drawExplorerLocation(explorerLocation, theMoveOutcome.location);
-            explorerLocation = theMoveOutcome.location;
+    this.stopMoving = function stopMoving()
+    {
+        $("#pauseButton").hide();
+        $("#goButton").show();
+        isMoving = false;
+    }
 
-            if(theMoveOutcome.exitReached)
-            {
-                LOG.updateLog("Exit reached!");
-                explorer.exitMaze();
-                endMazeTraversal();
-            }
-            LOG.updateLog("-------------------------");
-        })
-        .fail(function fail(err)
-        {
-            LOG.updateLog("Failed to move " + JSON.stringify(chosenDirection) + " from " + JSON.stringify(explorerLocation));
-            LOG.updateLog("-------------------------");
-        })
-        .done();
-    };
+    this.changeSpeed = function changeSpeed(delta)
+    {
+        moveDelayMs += delta;
+        moveDelayMs = Math.max(0, moveDelayMs);
+    }
 }
 
--- src/main/webapp/js/log.js
+++ src/main/webapp/js/log.js
@@ -1,11 +1,16 @@
 
 function Log()
 {
-    this.updateLog = function(contentLine)
+    this.updateLog = function updateLog(contentLine)
     {
         $("#logScroll").append("<p style='padding-left: 10px; margin:0'>" + contentLine + "</p>");
 
         var logScroll = document.getElementById("logScroll");
         logScroll.scrollTop = logScroll.scrollHeight;
+    }
+
+    this.clear = function clear()
+    {
+        $("#logScroll").html("");
     }
 }
